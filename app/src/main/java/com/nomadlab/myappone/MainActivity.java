13f1ddbe7208688441131b4abb90b67e0a66f936
questzz@naver.com <questzz@naver.com>
2021-12-14 06:27:25 +0900

    4일차 예제 수업

--- app/src/main/java/com/nomadlab/myappone/MainActivity.java
+++ app/src/main/java/com/nomadlab/myappone/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.frame_layout_01);
+        setContentView(R.layout.constraint_01);
+
+
     }
 }
